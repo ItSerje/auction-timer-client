286da2656925114ae3922025acd3e7778e5aed6e
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-12-13 18:22:27 +0600

    add getFormattedMinutesAndSeconds to utils

---
+++ src/utils.ts
@@ -0,0 +1,15 @@
+export const getFormattedMinutesAndSeconds = (time: number): string => {
+  // let h = time / 3600;
+  const hs = time % 3600;
+  let m: number | string = parseInt((hs / 60).toString());
+  let s: number | string = hs % 60;
+  if (m < 10) {
+    m = '0' + m;
+  }
+  if (s < 10) {
+    s = '0' + s;
+  }
+
+  // return { h, m, s };
+  return `${m}:${s}`;
+};
